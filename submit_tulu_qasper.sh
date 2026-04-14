d5f2b5d61852eac8af7104cc2896339261541612
AGNDM <eiresendeznickolas9@gmail.com>
2026-04-14 15:01:36 +1000

    sh

--- submit_tulu_qasper.sh
+++ submit_tulu_qasper.sh
@@ -6,7 +6,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --gpus-per-node=4
 #SBATCH --cpus-per-task=288
-#SBATCH --output=tulu_qasper_%j.out# 标准输出日志 (%j 为 Job ID)
+#SBATCH --output=tulu_qasper_%j.out # 标准输出日志 (%j 为 Job ID)
 #SBATCH --error=tulu_qasper_%j.err # 错误输出日志
 
 # 加载你的虚拟环境
@@ -22,7 +22,7 @@ export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 echo "Starting distributed training on 4 GPUs..."
 
 # 使用 torchrun 启动 4 卡训练
-srun --environment=$SCRATCH/share/xianrong_liu/tut/ngc-pytorch-25.06.toml bash -c \"
+srun --environment=$SCRATCH/share/xianrong_liu/tut/ngc-pytorch-25.06.toml bash -c "
     source $SCRATCH/share/xianrong_liu/myvenv/bin/activate && \
     source $SCRATCH/share/xianrong_liu/cache_env_setup.sh && \
     torchrun --nproc_per_node=4 finetune_instruct/tulu_qasper_finetune.py
