d2cfbc97b727ab092c3d6850b02eebc08d57106d
jiabao <12144118d@connect.polyu.hk>
2026-04-17 01:14:12 +1000

    add functions: generate_bot_reply(), chat()

---
+++ evaluation_ui/demo.py
@@ -0,0 +1,24 @@
+import gradio as gr
+
+# Invoke fintune model
+def generate_bot_reply(message:str, history: list[list[str]])-> str:
+    """
+    message: the latest user message
+    history: previous chat history in [[user, assistant], ...] format
+
+    Return:
+        assistant response as a string
+    """
+    return f"You said {message}"
+    pass
+
+# Chatbot logic function
+def chat(message: str, history: list[list[list]]):
+    """
+    Triggered when the user sends messages
+    """
+    if not message.strip():
+        return "", history
+    else:
+        reply = generate_bot_reply(message, history)
+        history = history + [[message, reply]]
