178dfb09f067be0eaebecd030f9dd63b8c48c10a
AGNDM <eiresendeznickolas9@gmail.com>
2026-04-17 23:57:04 +1000

    update dataset

--- finetune_instruct/tulu_qasper_finetune.py
+++ finetune_instruct/tulu_qasper_finetune.py
@@ -50,7 +50,7 @@ def main():
 
     # 3. LoRA Configuration
     lora_config = LoraConfig(
-        r=16,
+        r=128,
         lora_alpha=32,
         target_modules=["q_proj", "k_proj", "v_proj", "o_proj", "gate_proj", "up_proj", "down_proj"],
         lora_dropout=0.05,
