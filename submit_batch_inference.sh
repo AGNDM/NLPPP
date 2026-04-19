68ad5385eaecf5527e27d9ba05b2b717bdfd1352
AGNDM <eiresendeznickolas9@gmail.com>
2026-04-20 02:17:06 +1000

    sh

--- submit_batch_inference.sh
+++ submit_batch_inference.sh
@@ -12,9 +12,9 @@
 set -euo pipefail
 
 : "${NGC_ENV_FILE:=$SCRATCH/share/xianrong_liu/tut/ngc-pytorch-25.06.toml}"
-: "${PROJECT_DIR:=$tutorial_NLPPP}"
-: "${VENV_ACTIVATE:=$myvenv/bin/activate}"
-: "${CACHE_ENV_SETUP:=$cache_env_setup.sh}"
+: "${PROJECT_DIR:=tutorial_NLPPP}"
+: "${VENV_ACTIVATE:=myvenv/bin/activate}"
+: "${CACHE_ENV_SETUP:=cache_env_setup.sh}"
 
 cd $SCRATCH/share/xianrong_liu
 
